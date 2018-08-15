dabfb194237fe87edfb1173ec85f9da15a6380be
Max Tauro <maxtauro@hotmail.com>
2018-08-15 18:15:23 -0400

    Added basic user firebase functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,15 @@
+//TODO
+// - fix naming
+// - modularize the code properly
+// - encapsulate firebase methods
+
 (function () {
 
     'use strict';
-
+    // check if user is logged in
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-            alert("logged in");
+            // loadTasks();
             //TODO add login logic (load user data)
 
         } else { // No user is signed in
@@ -28,6 +33,47 @@
         span.className = "close";
         span.appendChild(txt);
         myNodelist[i].appendChild(span);
+    }
+
+
+    function signIn() {
+        var username = document.getElementById("uname").value;
+        var password = document.getElementById('psw').value;
+
+        document.getElementById("uname").value = '';
+        document.getElementById('psw').value = '';
+
+        firebase.auth().signInWithEmailAndPassword(username, password)
+            .catch(function(err) {
+                alert(err); //TODO put meaningful error response
+                return false;
+            });
+        return true;
+    }
+
+    function createUser() { //TODO refine user creation
+        var username = document.getElementById("new-username").value;
+        var password = document.getElementById('new-password').value;
+        var confirmPassword = document.getElementById('confirm-password').value;
+
+        //TODO should the username persist?
+        document.getElementById("new-username").value = '';
+        document.getElementById('new-password').value = '';
+        document.getElementById('confirm-password').value = '';
+
+        if (password === confirmPassword) {
+            // Register a new user // sick
+            firebase.auth().createUserWithEmailAndPassword(username, password)
+                .catch(function (err) {
+                    alert(err); //TODO put meaningful error response
+                    return false;
+                });
+        }
+        else {
+            alert("Passwords do not match"); //TODO make this cleaner
+        }
+
+        return true;
     }
 
     // Create a new task, add task to List
@@ -78,32 +124,62 @@
     var close = document.getElementsByClassName("close");
     var i;
     for (i = 0; i < close.length; ++i) {
-        close[i].onclick = function () {
+        close[i].addEventListener('click', function () {
             var div = this.parentElement;
             div.style.display = "none";
-        }
+        }, false);
     }
 
-    var addTaskInput = document.getElementById('addtask')
-    addTaskInput.onkeypress = function (e) {
+    var addTaskInput = document.getElementById('addtask');
+    addTaskInput.addEventListener("keypress", function (e) {
         if (!e) e = window.event;
         var keyCode = e.keyCode || e.which;
         if (keyCode == '13') {
             newTask();
             return false;
         }
-    }
+    }, false);
+
 
     //Close the signin modal if user clicks outside
     var modal = document.getElementById('loginFormModal');
-    window.onclick = function(event) {
+    window.addEventListener('click', function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
-    }
+    }, false);
 
-    a.onclick = function (event) {
-        alert("new user");
-    }
+    //Trigger firebase sign-in
+    var signInForm = document.getElementById('loginform');
+    signInForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // don't refresh when form submits
+
+        if (signIn()) {
+            var modal = document.getElementById('loginFormModal');
+            modal.style.display = "none";
+        }
+    }, false);
+
+    //New user link event listener
+    var newUserLink = document.getElementById('new-user');
+    newUserLink.addEventListener('click', function (event) {
+        var loginModal = document.getElementById('loginFormModal');
+        loginModal.style.display = "none";
+
+        var newUserModal  = document.getElementById('newUserModal');
+        newUserModal.style.display='block';
+        }, false);
+
+    // Trigger create User Event
+    var newUserForm = document.getElementById('newUserForm');
+    newUserForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // don't refresh when form submits
+
+        if (createUser()) {
+            var newUserModal  = document.getElementById('newUserModal');
+            newUserModal.style.display='none';
+        }
+    }, false);
+
 
 })();
